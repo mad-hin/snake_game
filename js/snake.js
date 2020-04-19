742487f11b8086ce8ad17126a504d1809c634807
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-19 20:53:16 +0800

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -1,5 +1,6 @@
 var $canvas = { element: null, width: null, height: null };
 var $ctx = null;
+var $grid = { maxRow: 20, maxCol: 28, unitSize: 20, unitGap: 5 };
 
 $(document).ready(function () {
     $canvas.element = document.getElementById('main-canvas');
