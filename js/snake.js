e674441dbc2337982686b754542a93df3e1a090f
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-19 20:48:47 +0800

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,11 @@
-function startgame() {
-    
-}
+var $canvas = { element: null, width: null, height: null };
+var $ctx = null;
+
+$(document).ready(function () {
+    $canvas.element = document.getElementById('main-canvas');
+    $canvas.element.width = $canvas.width;
+    $canvas.element.height = $canvas.height;
+    if ($canvas.element.getContext) {
+        $ctx = $canvas.element.getContext('2d');
+    }
+});
