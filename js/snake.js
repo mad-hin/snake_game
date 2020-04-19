a9c17483693b0499a5fbf468b30a2492142fe66b
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-19 20:58:53 +0800

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -1,12 +1,19 @@
+// grid part
 var $canvas = { element: null, width: null, height: null };
 var $ctx = null;
-var $grid = { maxRow: 20, maxCol: 28, unitSize: 20, unitGap: 5 };
+var $grid = { maxRow: 30, maxCol: 50, unitSize: 20, unitGap: 5 };
 
 $(document).ready(function () {
-    $canvas.element = document.getElementById('main-canvas');
-    $canvas.element.width = $canvas.width;
-    $canvas.element.height = $canvas.height;
+    init();
     if ($canvas.element.getContext) {
         $ctx = $canvas.element.getContext('2d');
     }
 });
+
+function init() {
+    $canvas.element = document.getElementById('main-canvas');
+    $canvas.width = $grid.maxCol * $grid.unitSize + ($grid.maxCol + 1) * $grid.unitGap;
+    $canvas.height = $grid.maxRow * $grid.unitSize + ($grid.maxRow + 1) * $grid.unitGap;
+    $canvas.element.width = $canvas.width;
+    $canvas.element.height = $canvas.height;
+}
